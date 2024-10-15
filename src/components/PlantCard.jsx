ab6b882bc60b5f3912ca82a3df654972b482d029
Aksh012 <akshata0802@gmail.com>
2024-10-15 12:41:33 +0530

    Akshats 4th commit

--- src/components/PlantCard.jsx
+++ src/components/PlantCard.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React from "react";
+import Favicon from "react-favicon";
 
-const PlantCard = ({ imageSrc, name, type, onBookmarkToggle, isBookmarked, onLearnMore }) => {
+
+const PlantCard = ({
+  imageSrc,
+  name,
+  type,
+  onBookmarkToggle,
+  isBookmarked,
+  onLearnMore,
+}) => {
   return (
     <div
-      className="bg-sec-color shadow-lg rounded-lg overflow-hidden w-full border border-gray-200 cursor-pointer pb-1 transition transform hover:scale-105 duration-500 ease-in-out" onClick={onLearnMore}  // Make the entire card clickable
+      className="bg-sec-color shadow-lg rounded-lg overflow-hidden w-full border border-gray-200 cursor-pointer pb-1 transition transform hover:scale-105 duration-500 ease-in-out"
+      onClick={onLearnMore} // Make the entire card clickable
     >
       {/* Image with reduced height */}
-      <img src={imageSrc} alt={name} className="w-full h-80 object-cover p-3 rounded-2xl" />
+      <img
+        src={imageSrc}
+        alt={name}
+        className="w-full h-80 object-cover p-3 rounded-2xl"
+      />
 
       {/* Plant Name and Type */}
       <div className="p-4 pt-1 bg-sec-color text-center">
-        <h3 className="text-2xl font-semibold text-gray-800 text-left">{name}</h3>
+        <h3 className="text-2xl font-semibold text-gray-800 text-left">
+          {name}
+        </h3>
         <p className="text-xs text-white text-left">{type}</p>
       </div>
 
@@ -20,19 +36,25 @@ const PlantCard = ({ imageSrc, name, type, onBookmarkToggle, isBookmarked, onLea
         <div className="absolute bottom-2 right-2 flex space-x-4 text-xl">
           {/* Bookmark Button */}
           <button
-            className={`p-1.5 mb-2 hover:bg-gray-100 hover:border-none rounded-md transition-colors duration-200 ${isBookmarked ? 'text-yellow-500' : 'text-gray-600 hover:text-yellow-500'}`}
+            className={`p-1.5 mb-2  hover:bg-gray-100 hover:border-none rounded-md transition-colors duration-200 ${
+              isBookmarked
+                ? "text-yellow-500"
+                : "text-black hover:text-yellow-500"
+            }`}
             onClick={(e) => {
-              e.stopPropagation();  // Prevent triggering the card click when clicking bookmark
+              e.stopPropagation(); // Prevent triggering the card click when clicking bookmark
               onBookmarkToggle(name);
             }}
           >
-            <i className={isBookmarked ? "fas fa-check" : "fas fa-bookmark"}></i>
+            <i
+              className={isBookmarked ? "fas fa-check" : "fas fa-bookmark"}
+            ></i>
           </button>
 
           {/* Share Button */}
           <button
-            className="mb-2 text-gray-600 hover:text-green-500 p-1.5 hover:bg-gray-100 hover:border-none rounded-md transition-colors duration-200"
-            onClick={(e) => e.stopPropagation()}  // Prevent triggering the card click
+            className="mb-2  text-white hover:text-green-500 p-1.5 hover:bg-gray-100 hover:border-none rounded-md transition-colors duration-200"
+            onClick={(e) => e.stopPropagation()} // Prevent triggering the card click
           >
             <i className="fas fa-share-alt"></i>
           </button>
